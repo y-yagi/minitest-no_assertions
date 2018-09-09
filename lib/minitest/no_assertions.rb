dbf5eb6a87309f6b59645f68632776577ef4b002
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-09-09 09:23:22 +0900

    allow to set behavior

--- lib/minitest/no_assertions.rb
+++ lib/minitest/no_assertions.rb
@@ -3,6 +3,16 @@ require "minitest"
 
 module Minitest
   module NoAssertions
+    class << self
+      def behavior=(behavior)
+        @behavior = behavior
+      end
+
+      def behavior
+        defined?(@behavior) ? @behavior : nil
+      end
+    end
+
     class Reporter < Minitest::StatisticsReporter
       def initialize(io = $stdout, options = {})
         super(io, options)
@@ -11,7 +21,17 @@ module Minitest
       def record(result)
         if result.passed? && result.assertions.zero?
           msg = "%s#%s has no assertions!!" % [result.klass, result.name]
-          raise StandardError, msg
+          warn(msg)
+        end
+      end
+
+      private
+
+      def warn(msg)
+        if Minitest::NoAssertions.behavior == :stderr
+          $stderr.puts(msg)
+        else
+          raise msg
         end
       end
     end
--- test/minitest/no_assertions_test.rb
+++ test/minitest/no_assertions_test.rb
@@ -8,6 +8,15 @@ class Minitest::NoAssertionsTest < Minitest::Test
     self.reporter << Minitest::NoAssertions::Reporter.new($stdout)
   end
 
+  def capture_stderr
+    io = StringIO.new
+    $stderr = io
+    yield
+    io.string
+  ensure
+    $stderr = STDERR
+  end
+
   def test_detect_no_assertions_test
     e = assert_raises StandardError do
       Minitest::Runnable.run_one_method(DummyTest, :no_assertion, reporter)
@@ -19,6 +28,16 @@ class Minitest::NoAssertionsTest < Minitest::Test
   def test_respect_original_exception
     Minitest::Runnable.run_one_method(DummyTest, :exception, reporter)
     assert true
+  end
+
+  def test_set_behavior
+    @original_behavior = Minitest::NoAssertions.behavior
+    Minitest::NoAssertions.behavior = :stderr
+
+    err = capture_stderr { Minitest::Runnable.run_one_method(DummyTest, :no_assertion, reporter) }
+    assert_equal "DummyTest#no_assertion has no assertions!!\n",err
+  ensure
+    Minitest::NoAssertions.behavior = @original_behavior
   end
 end
 
